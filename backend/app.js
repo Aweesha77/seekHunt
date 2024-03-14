3ed2ce3a59b9cb3a8865f6ab2f4a4bb3fc22a433
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-14 23:18:00 +0530

    adding files

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
 
 const app=express();
+
 dotenv.config({path :'./config/config.env'});
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL]
+}));
+
 export default app;
