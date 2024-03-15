e6ad60323848671fbd2748ac9b1479416fa60522
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-15 22:42:04 +0530

    adding files

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {register,login,logout,getuser} from "../controllers/userController.js";
+import {register,login,logout,getUser} from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
