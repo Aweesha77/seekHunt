55dd0bf8430096e73b695ae1d31a1bee0334eda1
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-15 22:49:37 +0530

    adding files

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
-import {getAllJobs,postJob} from "../controllers/jobController.js";
+import {deleteJob,getAllJobs,getMyJobs,getSingleJob,postJob,updateJob} from "../controllers/jobController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/getall", getAllJobs);
 router.post("/post", isAuthenticated, postJob);
-// router.get("/getmyjobs", isAuthenticated, getMyJobs);
-// router.put("/update/:id", isAuthenticated, updateJob);
-// router.delete("/delete/:id", isAuthenticated, deleteJob);
-// router.get("/:id", isAuthenticated, getSingleJob);
+router.get("/getmyjobs", isAuthenticated, getMyJobs);
+router.put("/update/:id", isAuthenticated, updateJob);
+router.delete("/delete/:id", isAuthenticated, deleteJob);
+router.get("/:id", isAuthenticated, getSingleJob);
 
 export default router;
