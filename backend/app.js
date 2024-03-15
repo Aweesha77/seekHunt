142a56c3dcc5c4b1a1284b2b594f959eef40fceb
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-15 06:01:59 +0530

    adding files

--- backend/app.js
+++ backend/app.js
@@ -6,22 +6,22 @@ import fileUpload from "express-fileupload";
 
 const app=express();
 
-dotenv.config({path :'./config/config.env'});
+dotenv.config({path :'./config/config.env'});   //load env variables to process.env
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
+    origin: [process.env.FRONTEND_URL],      //the server will allow requests from to this origin
+    method: ["GET", "POST", "DELETE", "PUT"], //HTTP methods that are allowed for cross-origin requests
+    credentials: true,                       //allow cookies to be sent from the frontend
 }));
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());                    //parse cookies from incoming requests
+app.use(express.json());                    //parse json from incoming requests
+app.use(express.urlencoded({ extended: true }));  //parse urlencoded data from incoming requests
+                                                //This line tells Express to use the express.urlencoded() middleware. This middleware function parses incoming requests with URL-encoded payloads and is based on body-parser.extended: true option allows for rich objects and arrays to be encoded into the URL-encoded format.If extended is false, the URL-encoded data will be parsed with the querystring library.
 app.use(
-    fileUpload({
-      useTempFiles: true,
-      tempFileDir: "/tmp/",
+    fileUpload({                           //configure fileUpload middleware
+      useTempFiles: true,                   //use temp files instead of memory for managing file uploads
+      tempFileDir: "/tmp/",               //directory for temp files
     })
   );
 
