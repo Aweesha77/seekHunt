7c65ba658725a8acd85cf49d510d951031658cfc
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-16 06:50:13 +0530

    adding files

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -67,4 +67,72 @@ export const postApplication = catchAsyncErrors(async (req, res, next) => {
     email,
     coverLetter,
     phone,
+    address,
+    applicantID,
+    employerID,
+    resume: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+  });
+  res.status(200).json({
+    success: true,
+    message: "Application Submitted!",
+    application,
+  });
+});
 
+export const employerGetAllApplications = catchAsyncErrors(
+  async (req, res, next) => {
+    const { role } = req.user;
+    if (role === "Job Seeker") {
+      return next(
+        new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
+      );
+    }
+    const { _id } = req.user;
+    const applications = await Application.find({ "employerID.user": _id });
+    res.status(200).json({
+      success: true,
+      applications,
+    });
+  }
+);
+
+export const jobseekerGetAllApplications = catchAsyncErrors(
+  async (req, res, next) => {
+    const { role } = req.user;
+    if (role === "Employer") {
+      return next(
+        new ErrorHandler("Employer not allowed to access this resource.", 400)
+      );
+    }
+    const { _id } = req.user;
+    const applications = await Application.find({ "applicantID.user": _id });
+    res.status(200).json({
+      success: true,
+      applications,
+    });
+  }
+);
+
+export const jobseekerDeleteApplication = catchAsyncErrors(
+  async (req, res, next) => {
+    const { role } = req.user;
+    if (role === "Employer") {
+      return next(
+        new ErrorHandler("Employer not allowed to access this resource.", 400)
+      );
+    }
+    const { id } = req.params;
+    const application = await Application.findById(id);
+    if (!application) {
+      return next(new ErrorHandler("Application not found!", 404));
+    }
+    await application.deleteOne();
+    res.status(200).json({
+      success: true,
+      message: "Application Deleted!",
+    });
+  }
+);
